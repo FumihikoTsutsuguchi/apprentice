e4dea7795e788ae666a0ded3eecb3c07ee85365e
FumihikoTsutsuguchi <fumisamu927@gmail.com>
2024-01-10 17:37:47 +0900

    [add]html 環境構築 デイトラ模写

---
+++ quest_html/app/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="c-header">
+        <h1>30DAYSトライアル</h1>
+        <nav class="global-nav">
+            <ul>
+                <li><a href="#what">デイトラとは</a></li>
+                <li><a href="#list">コース一覧</a></li>
+                <li><a href="#question">お問い合わせ</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="p-front__mv">
+            <img src="img/main-vsual.png" alt="">
+        </div>
+        <div class="c-container p-front">
+            <section class="p-front__section">
+                <h2 id="what">デイトラとは</h2>
+                <div class="p-front__what">
+                    <div>
+                        <img src="img/about.png" alt="">
+                    </div>
+                    <div>
+                        <p>デイトラとは、無料で・迷わず・楽しく学べるプログラム学習アプリです。毎日設定された課題をこなしていくだけで、未経験から30日でプログラミングスキルが身につきます。1日1題でプロのWebエンジニアを目指しましょう!
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section class="p-front__section">
+                <h2 id="list">コース一覧</h2>
+                <div class="p-front__course">
+                    <div>
+                        <img src="img/web_first.png" alt="">
+                        <p>HTML / CSS / Bootstrap</p>
+                    </div>
+                    <div>
+                        <img src="img/web_second.png" alt="">
+                        <p>HTML / CSS / JavaScript / jQuery</p>
+                    </div>
+                    <div>
+                        <img src="img/web_third.png" alt="">
+                        <p>PHP / WordPress</p>
+                    </div>
+                </div>
+            </section>
+            <section class="p-front__section">
+                <h2 id="question">お問い合わせ</h2>
+                <div class="p-front__question">
+                    <p>さあ今日から30日間を始めよう!</p>
+                    <form class="p-front__question-form">
+                        <input type="email" name="" id="" value="メールアドレス">
+                    </form>
+                    <div class="c-button"><a href="">無料ではじめる</a></div>
+                </div>
+            </section>
+        </div>
+    </main>
+    <footer class="c-container c-footer">
+        <p>Copyright(C) 2020 東京フリーランス ALL Rights Reserved.</p>
+    </footer>
+</body>
+</html>
